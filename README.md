49988b3aa4f39012203c33101e86117f0bd1436b
calliereimann <70546246+calliereimann@users.noreply.github.com>
2022-03-20 14:52:26 -0700

    Big update to dissemination barrier - fixed major correctness issue

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Benchmark Assignment:
 
 My program (compilation instructions: type make, and then use ./benchmark) had some bumps getting into a place where it would work properly. However, after switching to using the clock_gettime rather than clock() everything just seemed to click into place.
 
-While there's pretty significant overhead added due to the loop,
+While there's pretty significant overhead added due to the loop, this system still allows for analysis without having every single time measurement get rounded away to nothing.
 
 ![Memory Benchmark Graph](https://github.com/calliereimann/389memorythreadcode/blob/main/betterimage.png?raw=true)
 
@@ -23,4 +23,12 @@ Multithreading Assignment:
 I had some trouble coming up with a way of testing these things at first, but the (somewhat) clunky method I came up with is the best that I have.
 I apologize for the incompleteness: this is my literal first time using the <thread> library, and so a lot of time I would have had to work on the assignment was spent instead trying to understand how anything actually worked in the first place.
 
+Note: I do not optimize any of these threaded files - the optimization completely breaks them by smoothing away spinning and thus preventing the barriers from functioning properly.
+
 The central barrier was implemented essentially identically to how we set it up in class - I needed to make sense also considered "atomic" in order to get the code to compile, but once I did (and I figured out how to pass references into threads) it worked quite well.
+
+The central barrier is correct - in testing, there has never been any out-of-order effects. This is because every variable related to the behavior of the barrier is an atomic and so there's no effects which could lead to disasters. Note: The printing might get screwed up from time to time, that's just the fault of cout transmitting each chunk in pieces. notably, the first chunk of any line in the second barrier will always appear after the last chunk in the first barrier.
+
+The dissemination barrier is also correct. I needed to move sense and parity to be local per thread in order to make it work, since there's not really a priveliged "last one to arrive" to flip them, so i just had everyone have their own that they personally flipped. As per the reasoning in the slides, this barrier is now totally correct.
+
+I don't have a good graph available because I don't know how to measure this sort of thing, but if I had to guess, central barrier has essentially a flat line overhead as it's all down to a single value, while dissemination barrier has n log n overhead because it creates THREAD * log(THREAD) * 2 different flags in a bool array.
--- barrier_test.cc
+++ barrier_test.cc
@@ -7,8 +7,8 @@
 #include <vector>
 
 std::atomic<int> counter = 1; //some fun constants and global atomics
-const int THREADC = 4;
-const int ROUNDS = log2(THREADC); //dissemination wouldn't work until i pulled these out into being almost globals
+const int THREADC = 5;
+const int ROUNDS = log2(THREADC)+1; //dissemination wouldn't work until i pulled these out into being almost globals
 
 
 using id_type = std::thread::id;
@@ -29,7 +29,7 @@ void threadCentral(int threadc, std::atomic<bool> &sense){
         std::chrono::milliseconds delay (rand()%30);
         std::this_thread::sleep_for(delay);
         joincentral(std::this_thread::get_id(), threadc, sense);
-        delay = std::chrono::milliseconds{(rand()%50)};
+        delay = std::chrono::milliseconds{rand()%50};
         std::cout<<"thread "<<std::this_thread::get_id()<<" through first barrier\n";
         delay = std::chrono::milliseconds{(rand()%50)};
         std::this_thread::sleep_for(delay);
@@ -67,33 +67,34 @@ void centraltest(){
          */
 }
 
-id_type joindissemination(id_type id, int threadc, int position, std::atomic<bool> &sense, std::atomic<int> &parity, std::atomic<bool> flags[][2][ROUNDS]){
+id_type joindissemination(id_type id, int position, std::atomic<bool> &sense, std::atomic<int> &parity, std::atomic<bool> flags[THREADC][2][ROUNDS]){
         for (int i = 0; i < ROUNDS; i++) {
-                auto partner = (position + (2^i)) % threadc;
+                int partner = (position + (2^i)) % THREADC;
                 flags[partner][parity][i] = !sense;
                 while (flags[position][parity][i] == sense) { /* spin */ }
         } if (parity == 1) { sense = !sense;} parity = 1 - parity;
         return id;
 }
 
-void threadDissemination(int threadc, int position, std::atomic<bool> &sense, std::atomic<int> &parity, std::atomic<bool> flags[][2][ROUNDS]){ //"wait, join barrier, wait, join barrier, repeat"
+void threadDissemination(int position, std::atomic<bool> flags[THREADC][2][ROUNDS]){ //"wait, join barrier, wait, join barrier, repeat"
+        std::atomic<bool> sense = false;
+        std::atomic<int> parity = 0;
         std::chrono::milliseconds delay (rand()%30);
         std::this_thread::sleep_for(delay); //looks like a mess but is essentially the same four lines copied three times
-        joindissemination(std::this_thread::get_id(), threadc, position, sense, parity, flags);
-        std::cout<<"thread "<<std::this_thread::get_id()<<" through first barrier\n";
+        joindissemination(std::this_thread::get_id(), position, sense, parity, flags);
+        std::cout<<"1thread "<<std::this_thread::get_id()<<" through first barrier\n";
         delay = std::chrono::milliseconds{(rand()%70)};
         std::this_thread::sleep_for(delay);
-        joindissemination(std::this_thread::get_id(), threadc, position, sense, parity, flags);
-        std::cout<<"thread "<<std::this_thread::get_id()<<" through second barrier\n";
+        joindissemination(std::this_thread::get_id(), position, sense, parity, flags);
+        std::cout<<"2thread "<<std::this_thread::get_id()<<" through second barrier\n";
         delay = std::chrono::milliseconds{(rand()%70)};
         std::this_thread::sleep_for(delay);
-        joindissemination(std::this_thread::get_id(), threadc, position, sense, parity, flags);
-        std::cout<<"thread "<<std::this_thread::get_id()<<" through third barrier\n";
+        joindissemination(std::this_thread::get_id(), position, sense, parity, flags);
+        std::cout<<"3thread "<<std::this_thread::get_id()<<" through third barrier\n";
 }
 
 void disseminationtest(){
-        std::atomic<bool> sense = false;
-        std::atomic<int> parity = 0;
+
         std::atomic<bool> flags[THREADC][2][ROUNDS]; //allocated in local storage per thread
         for (int i = 0; i < THREADC; i++) {
                 for (int j = 0; j < 2; j++) {
@@ -104,7 +105,7 @@ void disseminationtest(){
         }
         std::vector<std::thread> threads;
         for (int i = 0; i < THREADC; i++) {
-                threads.push_back(std::thread(threadDissemination, THREADC, i, std::ref(sense), std::ref(parity), std::ref(flags)));
+                threads.push_back(std::thread(threadDissemination, i, std::ref(flags)));
         }
         /*
            std::thread one (threadAction, 5, std::ref(sense));
